13d4a63630b21edaaaf9e060c21a77cac971e0e6
april <imlimpoism@gmail.com>
2025-11-08 13:45:49 -0400

    nothing added.

--- main.asm
+++ main.asm
@@ -4,9 +4,9 @@ segment readable executable
 entry start
 start:
 	lea	rsi,	[msg] ; LOADS THE EFFECTIVE ADDRESS OF MESSAGE
-	mov rdx,	sz	  ; MOVE SIZE INTO RDX
+	mov	rdx,	sz	  ; MOVE SIZE INTO RDX
 	mov	rdi,	1	  ; FILE DESCRIPTOR (SDTOUT)
-	mov rax,	1	  ; SYSTEM CALL (SYSWRITE)
+	mov	rax,	1	  ; SYSTEM CALL (SYSWRITE)
 	syscall
 	xor	rdi,	rdi
 	mov	rax,	60
